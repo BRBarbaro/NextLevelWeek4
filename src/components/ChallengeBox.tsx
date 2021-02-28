f7d7519ada85c7aa284e83f76f5f1b087a3d1b64
Bruno Barbaro <bruno.barbaro1995@gmail.com>
2021-02-28 14:30:10 -0300

    NLW4 - Aula 4

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox () {
 
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge, completeChallange } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceeded() {
+    completeChallange();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
     
   return(
     <div className={styles.challengeBoxContainer}>
@@ -18,10 +30,10 @@ export function ChallengeBox () {
             <p>{ activeChallenge.description }</p>
           </main>
           <footer>
-            <button type="button" className={styles.challengeBoxFailedButton} onClick={resetChallenge}> 
+            <button type="button" className={styles.challengeBoxFailedButton} onClick={handleChallengeFailed}> 
               Falhei
             </button>
-            <button type="button" className={styles.challengeBoxSucceededButton}>
+            <button type="button" className={styles.challengeBoxSucceededButton} onClick={handleChallengeSucceeded}>
               Completei
             </button>
           </footer>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useState } from 'react';
+import { count } from 'console';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -20,6 +21,7 @@ interface ChallengesContextData {
   experienceToNextLevel: number;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallange: () => void;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -33,17 +35,48 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, [])
+
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
-    setActiveChallenge(challenges[randomChallengeIndex]);
+    const challenge = challenges[randomChallengeIndex]
+    setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo desafio', {
+        body: `Valendo ${challenge.amount} xp!`
+      })
+    }
   }
 
   function resetChallenge() {
     setActiveChallenge(null);
+  }
+
+  function completeChallange() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
   }
 
   return (
@@ -56,7 +89,8 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
         challengesCompleted, 
         experienceToNextLevel,
         startNewChallenge,
-        resetChallenge }}>
+        resetChallenge,
+        completeChallange }}>
       {children}
     </ChallengesContext.Provider>
   )
